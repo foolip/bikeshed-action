7dc24660501fff4ac3ce0be0e24fa210c11a0970
Philip Jägenstedt <philip@foolip.org>
2020-06-01 10:05:05 +0200

    Use **/*.bs glob for input

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const core = require('@actions/core');
 const fs = require('fs').promises;
+const glob = require('@actions/glob');
 
 async function run() {
   try {
     const src = core.getInput('src');
-    console.log(`Building ${src} ...`);
-
-    const body = await fs.readFile(src, 'utf8');
-    console.log(body);
+    console.log(`Considering ${src}`);
+    const globber = await glob.create(src);
+    const files = await globber.glob();
+    console.log(`Found ${files.length} file(s)`);
+    for (const file of files) {
+      console.log(`${file} contents:`);
+      const body = await fs.readFile(file, 'utf8');
+      console.log(body);
+    }
   }
   catch (error) {
     core.setFailed(error.message);
